072daaa695e14bc31a4335dbe8b2759bce0da1fb
Layna Oyervides <laynaoyervides@gmail.com>
2021-07-17 22:58:54 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop("Garfield");
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift("Milo");
+}
+function appendCat(name){
+    const copyCats = [...cats, "Broom"];
+    return copyCats;
+}
+function prependCat(name){
+    const moreCats = ["Arnold", ...cats];
+    return moreCats;
+}
+function removeLastCat(name){
+    return (cats.slice(0,2));
+}
+function removeFirstCat(name){
+    return (cats.slice(1,3));
+}
